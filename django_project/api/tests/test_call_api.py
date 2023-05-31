6817152b16fc3f36a78b1b74eb75ec619edccfe3
Gabriel <gabrigomez15@gmail.com>
2023-05-31 10:31:59 -0300

    feat: add a test example to check create method on api

--- django_project/api/tests/test_call_api.py
+++ django_project/api/tests/test_call_api.py
@@ -5,3 +5,16 @@ ENDPOINT = "https://todo.pixegami.io"
 def test_call_api():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
+
+def test_can_create_task():
+    payload = {
+        "content": "my test",
+        "user_id": "mocoto",
+        "task_id": "test_id_666",
+        "is_done": "false",
+    }
+    response = requests.put(ENDPOINT + "/create-task", json=payload)
+    assert response.status_code == 200
+
+    data = response.json()
+    print(data)
