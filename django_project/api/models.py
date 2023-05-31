d9a69a8126b590aa8432131bda898834af478252
Gabriel <gabrigomez15@gmail.com>
2023-05-31 10:47:30 -0300

    feat: add user model in models

--- django_project/api/models.py
+++ django_project/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.CharField(unique=True)
+    password = models.CharField(max_length=25)
+    
