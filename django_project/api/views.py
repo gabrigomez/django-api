043462373f5d9ab83b8c6f038500a57f7049ca72
Gabriel <gabrigomez15@gmail.com>
2023-05-31 11:14:42 -0300

    feat: add UserView in Views

--- django_project/api/views.py
+++ django_project/api/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer
+from .models import User
 from django.http import HttpResponse
 
 # Create your views here.
 def main(request):
     return HttpResponse('Working')
+
+class UserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
