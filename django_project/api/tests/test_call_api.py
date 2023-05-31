737eb204fc8bc8f9552f09f9032ed1474fc53416
Gabriel <gabrigomez15@gmail.com>
2023-05-31 11:32:22 -0300

    fix: add local endpoint in unit tests

--- django_project/api/tests/test_call_api.py
+++ django_project/api/tests/test_call_api.py
@@ -1,9 +1,11 @@
 import requests
 
 ENDPOINT = "https://todo.pixegami.io"
+LOCAL_ENDPOINT = "http://127.0.0.1:8000"
+
 
 def test_call_api():
-    response = requests.get(ENDPOINT)
+    response = requests.get(LOCAL_ENDPOINT + '/user')
     assert response.status_code == 200
 
 def test_can_create_task():
