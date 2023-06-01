e774dcddf8a105799c6ec7118a43292eaed0904b
Gabriel <gabrigomez15@gmail.com>
2023-06-01 17:28:08 -0300

    fix: clean tests file

--- django_project/api/tests/test_call_api.py
+++ django_project/api/tests/test_call_api.py
@@ -2,14 +2,13 @@ import requests
 import random
 import string
 
-ENDPOINT = "https://todo.pixegami.io"
-LOCAL_ENDPOINT = "http://127.0.0.1:8000"
+ENDPOINT = "http://127.0.0.1:8000"
 
 def random_name(char_num):
     return ''.join(random.choice(string.ascii_letters) for _ in range(char_num))
 
 def test_call_api():
-    response = requests.get(LOCAL_ENDPOINT + '/user')
+    response = requests.get(ENDPOINT + '/user')
     assert response.status_code == 200
 
 def test_can_post_user():
@@ -22,32 +21,19 @@ def test_can_post_user():
         "username": f"{username}",
         "password": "askovzinha123"
     }
-    response = requests.post(LOCAL_ENDPOINT + "/create-user", json=payload)
+    response = requests.post(ENDPOINT + "/create-user", json=payload)
     assert response.status_code == 201
 
     data = response.json()
     print(data)
 
 def test_can_find_user():
-    response = requests.get(LOCAL_ENDPOINT + '/user/9')
+    response = requests.get(ENDPOINT + '/user/9')
     assert response.status_code == 200
 
 def test_can_delete_user():
-    response = requests.delete(LOCAL_ENDPOINT + '/user/1')
+    response = requests.delete(ENDPOINT + '/user/1')
     assert response.status_code == 201
 
-    response = requests.get(LOCAL_ENDPOINT + '/user/1')
+    response = requests.get(ENDPOINT + '/user/1')
     assert response.status_code == 404
-
-def test_can_create_task():
-    payload = {
-        "content": "my test",
-        "user_id": "mocoto",
-        "task_id": "test_id_666",
-        "is_done": "false",
-    }
-    response = requests.put(ENDPOINT + "/create-task", json=payload)
-    assert response.status_code == 200
-
-    data = response.json()
-    print(data)
