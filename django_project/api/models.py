1b37ae4a27feb76a5e1bab644ffe15d3492ac241
Gabriel <gabrigomez15@gmail.com>
2023-06-02 10:14:45 -0300

    fix: alter again user password length in database

--- django_project/api/models.py
+++ django_project/api/models.py
@@ -6,6 +6,6 @@ class User(models.Model):
     id_user = models.AutoField(primary_key=True)
     username = models.CharField(max_length=30, unique=True)
     email = models.CharField(unique=True)
-    password = models.CharField(max_length=40)
+    password = models.CharField(max_length=100)
     language = models.TextField(max_length=20, default='')
     
